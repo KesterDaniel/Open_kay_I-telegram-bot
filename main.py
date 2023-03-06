2f518952a0a0908af3e94461ab8a243e936e97a5
KesterDaniel <kesterdan17@gmail.com>
2023-03-06 16:12:46 +0100

    add git hub actions

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Welcome to Open Kay-I, created by kester. Do well to ask reasonable questions and dont you"
-                          "dare try to say shit...LOL")
+                          " dare try to say shit...LOL")
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
